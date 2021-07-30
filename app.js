df3f0ea6a23bb6f6b1e3a0bbaa0d9ba2d05d9690
Prakhar Saxena <75561866+SkyWalkerPS@users.noreply.github.com>
2021-07-30 19:08:48 +0530

    Download feature added

--- app.js
+++ app.js
@@ -12,6 +12,38 @@ document.getElementById('clear-canvas').addEventListener('click', function () {
 });
 
 
+
+//download feature
+const myCanvas = document.getElementsByTagName('canvas');
+const btnDownload = document.getElementById('download');
+
+btnDownload.addEventListener('click', ()=>{
+  const dataURI = myCanvas[0].toDataURL();
+  document.getElementById('downloadable').src = dataURI;
+  document.getElementById('linker').href = dataURI;
+})
+
+
+
+
+
+// Eraser Feature
+var f=1;
+document.getElementById('Erase').addEventListener('click', function () {
+  if(f==1){
+    document.getElementById('Erase').innerHTML="Untoggle Eraser";
+    f=0;
+  }
+  else{
+    document.getElementById('Erase').innerHTML="Toggle Eraser";
+    f=1;
+  }
+});
+
+
+
+
+//P5 functions
 function setup() {
   let x =screen.width;
   let y = screen.height;
@@ -24,10 +56,16 @@ function draw() {
 }
 
 function mouseDragged() {
-  stroke(255);
+  if(f==1)stroke(255);
+  else stroke(0);
+  // stroke(255);
   strokeWeight(value);
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
+
+
+var canva = document.getElementsByTagName('canvas');
+console.log(canva[0]);
 
 
 
