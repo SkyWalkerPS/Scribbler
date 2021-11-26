932080760e4c76e03c44f42f23b7dd716aa1262d
Prakhar Saxena <75561866+SkyWalkerPS@users.noreply.github.com>
2021-11-26 13:15:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ document.getElementById('Erase').addEventListener('click', function () {
 function setup() {
   let x =screen.width;
   let y = screen.height;
-  createCanvas(y, x);
+  createCanvas(x, y);
   background("black");
 }
 
